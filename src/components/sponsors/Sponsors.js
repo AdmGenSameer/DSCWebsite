fb0457ae00d88f7634f952a5d6aefd98e24c95d6
prem-ramamoorthy <pkrprem2005@gmail.com>
2025-11-23 20:51:16 +0530

    image updated to use cloudinary links

--- src/components/sponsors/Sponsors.js
+++ src/components/sponsors/Sponsors.js
@@ -6,13 +6,13 @@ function Sponsors() {
   const ref = useRef();
 
   const sponsors = [
-    { name: 'Geeks for Geeks', logo: '/sponsors/gfg.png' },
-    { name: 'Coding Ninjas', logo: '/sponsors/codingNinjas.jpeg' },
-    { name: 'Interview Cake', logo: '/sponsors/ic.png' },
-    { name: 'NSE', logo: '/sponsors/nse.png' },
-    { name: 'Physics Wallah', logo: '/sponsors/physics-wallah.jpg' },
-    { name: 'UN stop', logo: '/sponsors/un.jpg' },
-    { name: 'CloudyML', logo: '/sponsors/ml.jpeg' },
+    { name: 'Geeks for Geeks', logo: 'https://res.cloudinary.com/dwpdekzdb/image/upload/v1763910818/gfg_f2kkgj.png' },
+    { name: 'Coding Ninjas', logo: 'https://res.cloudinary.com/dwpdekzdb/image/upload/v1763910829/codingNinjas_icireq.jpg' },
+    { name: 'Interview Cake', logo: 'https://res.cloudinary.com/dwpdekzdb/image/upload/v1763910841/ic_e0tvxn.png' },
+    { name: 'NSE', logo: 'https://res.cloudinary.com/dwpdekzdb/image/upload/v1763910858/nse_jdkn7z.png' },
+    { name: 'Physics Wallah', logo: 'https://res.cloudinary.com/dwpdekzdb/image/upload/v1763910869/physics-wallah_nmnwsf.jpg' },
+    { name: 'UN stop', logo: 'https://res.cloudinary.com/dwpdekzdb/image/upload/v1763910879/un_j8at3e.jpg' },
+    { name: 'CloudyML', logo: 'https://res.cloudinary.com/dwpdekzdb/image/upload/v1763910883/ml_zljjmv.jpg' },
 
   ];
 
