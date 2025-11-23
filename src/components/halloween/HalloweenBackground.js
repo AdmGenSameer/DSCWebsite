cfdf9c2d9f193d02b319aa159a4bb75e9f9807c3
AdmGenSameer <sameer00324@gmail.com>
2025-11-23 23:42:42 +0530

    fix: distribute pumpkins throughout scrollable page and remove bottom spider webs

--- src/components/halloween/HalloweenBackground.js
+++ src/components/halloween/HalloweenBackground.js
@@ -5,7 +5,7 @@ import InteractivePumpkin from './InteractivePumpkin';
 import SpiderWeb from './SpiderWeb';
 
 function HalloweenBackground({ children }) {
-  // Strategic pumpkin positions - distributed throughout the page
+  // Strategic pumpkin positions - distributed throughout the scrollable page
   const pumpkinPositions = [
     { id: 'pumpkin-1', top: '25vh', left: '8%', size: 'medium', mobile: true },
     { id: 'pumpkin-2', top: '45vh', left: '90%', size: 'large', mobile: true },
@@ -18,7 +18,7 @@ function HalloweenBackground({ children }) {
     },
     {
       id: 'pumpkin-4',
-      top: '75vh',
+      top: '120vh',
       left: '88%',
       size: 'medium',
       mobile: false,
@@ -27,6 +27,34 @@ function HalloweenBackground({ children }) {
       id: 'pumpkin-5',
       top: '35vh',
       left: '50%',
+      size: 'small',
+      mobile: false,
+    },
+    {
+      id: 'pumpkin-6',
+      top: '150vh',
+      left: '12%',
+      size: 'large',
+      mobile: false,
+    },
+    {
+      id: 'pumpkin-7',
+      top: '200vh',
+      left: '85%',
+      size: 'small',
+      mobile: false,
+    },
+    {
+      id: 'pumpkin-8',
+      top: '250vh',
+      left: '15%',
+      size: 'medium',
+      mobile: false,
+    },
+    {
+      id: 'pumpkin-9',
+      top: '300vh',
+      left: '90%',
       size: 'small',
       mobile: false,
     },
@@ -41,11 +69,9 @@ function HalloweenBackground({ children }) {
         <div className={styles.vignette} />
       </div>
 
-      {/* Spider webs in corners */}
+      {/* Spider webs in corners - only top corners */}
       <SpiderWeb position="top-left" />
       <SpiderWeb position="top-right" />
-      <SpiderWeb position="bottom-left" />
-      <SpiderWeb position="bottom-right" />
 
       {/* Flying bats (ambient) */}
       <FlyingBats count={8} />
